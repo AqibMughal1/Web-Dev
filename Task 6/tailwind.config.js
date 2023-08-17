1ad4836362f92a4819cfca52292383d02fc4813f
AQIBMUGHAL1 <fa21-bcs-073@cuilahore.edu.pk>
2023-08-17 16:16:03 +0500

    task 6

---
+++ Task 6/tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+    theme: {
+      extend: {
+        screens: {
+          'sm': '640px',
+          'md': '768px',
+          'lg': '1024px',
+          'xl': '1280px', 
+          'xxl': '1536px',
+        },
+      },
+    },
+    variants: {},
+    plugins: [],
+  }
+  
